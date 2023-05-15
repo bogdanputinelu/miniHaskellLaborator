20216cc38c1e61e17d0acefeb9a694dae314fd9f
bogdanputinelu <bogdanputinelu@yahoo.com>
2023-05-15 18:42:08 +0300

    curry typed

--- lab7/CurryHowardUntyped.hs
+++ lab7/CurryHowardUntyped.hs
@@ -1,101 +1,95 @@
 import Prelude (undefined)
+import Distribution.Compat.Lens (_1)
 
-data False                                        -- empty type
-
-data True = True                                  -- unit type
-
-data And a b = And { proj1 :: a, proj2 :: b }     -- product
-
-data Or a b                                       -- sum
-  = Left a
-  | Right b
-
+newtype False = False { getFalse :: forall t. t }
+newtype True = True { getTrue :: forall t . t -> t }
+newtype And a b = And { getAnd :: forall t. (a -> b -> t) -> t }
+newtype Or a b = Or { getOr :: forall t . (a -> t) -> (b -> t) -> t}
 type Not a = a -> False
 type Iff a b = And (a -> b) (b -> a)
+
 
 -- Natural deduction introduction and elimination rules
 
 trueIntro :: True                                   -- true introduction
-trueIntro = True
+trueIntro = True (\x -> x)
 
 falseElim :: False -> b                             -- false elimination
-falseElim x = case x of
+falseElim f = getFalse f 
 
 implIntro :: (a -> b) -> (a -> b)                   -- implication introduction
-implIntro f = f
+implIntro = undefined
 
 implElim :: (a -> b) -> a -> b                      -- implication elimination
-implElim f = f
+implElim = undefined
 
 andIntro :: a -> b -> And a b                       -- and introduction
-andIntro a b = And a b 
+andIntro = undefined
 
 andElimL :: And a b -> a                            -- and elimination 1
-andElimL and = proj1 and
+andElimL = undefined
 
 andElimR :: And a b -> b                            -- and elimination 2
-andElimR and = proj2 and 
+andElimR = undefined
 
 orIntroL :: a -> Or a b                             -- or introduction 1
-orIntroL a = Left a
+orIntroL = undefined
 
 orIntroR :: b -> Or a b                             -- or introduction 2
-orIntroR b = Right b
+orIntroR = undefined
 
 orElim :: Or a b -> (a -> c) -> (b -> c) -> c       -- or elimination
-orElim orAB fac fbc = case orAB of
-  Left a -> fac a
-  Right b -> fbc b
+orElim = undefined
 
 notElim :: Not p -> p -> c                          -- not elimination 
-notElim notA a = falseElim (notA a)
+notElim = undefined
 
 notIntro :: (forall p. a -> p) -> Not a             -- not introduction
-notIntro f = f
+notIntro _ = undefined
 
 iffIntro :: (a -> b) -> (b -> a) -> Iff a b         -- iff introduction
-iffIntro fab fba = And fab fba 
+iffIntro = undefined
 
 iffElimL :: Iff a b -> a -> b                       -- iff elimination 1
-iffElimL (And fab _fba) a = fab a
+iffElimL = undefined
 
 iffElimR :: Iff a b -> b -> a                       -- iff elimination 1
-iffElimR (And _fab fba) b = fba b
+iffElimR = undefined
 
 -- Hilbert-style axiomatization for intuitionistic propositional logic
 
 ax1 :: a -> b -> a
-ax1 a b = implIntro (\a -> implIntro (\b -> a)) a b 
+ax1 = undefined
 
 ax2 :: (a -> b) -> (a -> (b -> c)) -> a -> c
-ax2 f g a = implIntro (\a -> implIntro (\g -> implIntro (\a -> implElim (implElim g a) (implElim f a)))) f g a 
+ax2 = undefined
 
 ax3 :: a -> b -> And a b
-ax3 = implIntro (\a -> implIntro (\b -> andIntro a b)) a b 
+ax3 = undefined
 
 ax4 :: And a b -> a
-ax4 = andElimL
+ax4 = undefined
 
 ax5 :: And a b -> b
-ax5 = andElimR
+ax5 = undefined
 
 ax6 :: a -> Or a b
-ax6 = orIntroL
+ax6 = undefined
 
 ax7 :: b -> Or a b
-ax7 = orIntroR
+ax7 = undefined
 
 ax8 :: (a -> c) -> (b -> c) -> Or a b -> c
-ax8 ac bc ab = orElim ab ac bc
+ax8 = undefined
 
 ax9 :: (a -> b) -> (a -> Not b) -> Not a
-ax9 ab anb = notIntro (\a -> notElim (implElim anb a) (implElim ab a))
+ax9 = undefined
 
 ax10 :: Not a -> a -> b
-ax10 na a = notElim na a
+ax10 = undefined
 
 modusPonens :: (a -> b) -> a -> b
-modusPonens f = f 
+modusPonens = undefined
 
 -- Several tautologies
 
@@ -103,7 +97,7 @@ pNPFalse :: p -> Not p -> False
 pNPFalse = undefined
 
 deMorgan1 :: And (Not p) (Not q) -> Not (Or p q)
-deMorgan1 = implIntro (\andn -> (\or -> orElim or (andElimL andn) (andElimR andn)))
+deMorgan1 = undefined
 
 deMorgan2 :: Not (Or p q) -> And (Not p) (Not q)
 deMorgan2 = undefined
@@ -111,20 +105,8 @@ deMorgan2 = undefined
 deMorgan3 :: Or (Not p) (Not q) -> Not (And p q)
 deMorgan3 = undefined
 
-type DeMorgan4 = forall p q . Not (And p q) -> Or (Not p) (Not q)
+excludedMiddleImplDoubleNeg :: Or a (Not a) -> (Not (Not a) -> a)
+excludedMiddleImplDoubleNeg = undefined
 
--- Classical axioms
-
-type ExcludedMiddle = forall a. Or a (Not a)
-type DoubleNegation = forall a. Not (Not a) -> a
-type PeirceLaw = forall p q. ((p -> q) -> p) -> p
-
-excludedMiddleImplDoubleNeg :: ExcludedMiddle -> DoubleNegation
-excludedMiddleImplDoubleNeg em = undefined
-
-doubleNegImplExcludedMiddle :: DoubleNegation -> ExcludedMiddle -- hard
+doubleNegImplExcludedMiddle :: (forall a. Not (Not a) -> a) -> Or b (Not b)
 doubleNegImplExcludedMiddle dn = undefined
-
-classicDeMorgan4 :: ExcludedMiddle -> DeMorgan4
-classicDeMorgan4 em = undefined
-
